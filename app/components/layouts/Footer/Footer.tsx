3afca84a43971891d51f09f9db297f5ac0243015
273* <keita.y310@icloud.com>
2024-02-08 17:44:08 +0900

    メインページの要素を作成しました．

---
+++ app/components/layouts/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>273doworks</p>
+      <a href="http://webservice.recruit.co.jp/">
+        <img
+          src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif"
+          alt="ホットペッパーグルメ Webサービス"
+          width="135"
+          height="17"
+          title="ホットペッパーグルメ Webサービス"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/layouts/Index.ts
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+export { Header, Footer };
+
+// 共通のレイアウトを一括で読み込めるようにエクスポート
