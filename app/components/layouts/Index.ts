a97cc5810b99b4f0cc3b4e49de916179cbc02498
273* <keita.y310@icloud.com>
2024-02-08 18:27:07 +0900

    表示をプロット通りにしました．

--- app/components/layouts/Index.ts
+++ app/components/layouts/Index.ts
@@ -1,6 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
-export { Header, Footer };
-
 // 共通のレイアウトを一括で読み込めるようにエクスポート
+export { Header, Footer };
--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,41 @@ export default function Home() {
     latitude: 0,
     longitude: 0,
   });
+  // 検索フィルターの状態を保持する変数
+  const [filters, setFilters] = useState({
+    wifi: 0,
+    wedding: 0,
+    course: 0,
+    free_drink: 0,
+    free_food: 0,
+    private_room: 0,
+    horigotatsu: 0,
+    tatami: 0,
+    cocktail: 0,
+    shochu: 0,
+    sake: 0,
+    wine: 0,
+    card: 0,
+    non_smoking: 0,
+    charter: 0,
+    ktai: 0,
+    parking: 0,
+    barrier_free: 0,
+    sommelier: 0,
+    night_view: 0,
+    open_air: 0,
+    show: 0,
+    equipment: 0,
+    karaoke: 0,
+    band: 0,
+    tv: 0,
+    lunch: 0,
+    midnight: 0,
+    midnight_meal: 0,
+    english: 0,
+    pet: 0,
+    child: 0,
+  });
 
   // useRouterを仕様するための変数
   const router = useRouter();
@@ -35,7 +70,7 @@ export default function Home() {
     setCurrentLocation({ latitude, longitude });
   };
 
-  // 現在地を取得する処理が失敗したときの処理
+  // 現在地の取得が失敗したときの処理
   const errorCallback = (error: GeolocationPositionError) => {
     setAvailable(false);
   };
@@ -64,17 +99,22 @@ export default function Home() {
   return (
     <div className={`App ${inter.className}`}>
       <layout.Header />
-      <p>main</p>
-      <input
-        type="range"
-        min="0"
-        max="4"
-        value={selectDistance}
-        onChange={(e) => handleSliderChange(e)}
-        onMouseUp={() => handleMouseUp()}
-      />
-      <p>{distances_data[selectDistance]}m</p>
-      <p>{!isAvailable && "非表示"}</p>
+      <div>
+        <p>レストランを見つけましょう。</p>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="4"
+            value={selectDistance}
+            onChange={(e) => handleSliderChange(e)}
+            onMouseUp={() => handleMouseUp()}
+          />
+          <p>現在地からの検索半径を指定　{distances_data[selectDistance]}m</p>
+        </div>
+        <p>{!isAvailable && "非表示"}</p>
+        <p>検索オプション</p>
+      </div>
       <layout.Footer />
     </div>
   );
