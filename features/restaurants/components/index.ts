3f86eb3abb7cbbeaff04401e79282a7bde0e4e33
273* <114457271+273Do@users.noreply.github.com>
2024-02-13 16:35:59 +0900

    Delete features/restaurants/components directory

--- features/restaurants/components/index.ts
+++
@@ -1,5 +0,0 @@
-import RestaurantCard from "./RestaurantCard/RestaurantCard";
-import RestaurantList from "./RestaurantList/RestaurantList";
-
-// コンポーネントを一括で読み込めるようにエクスポート
-export { RestaurantCard, RestaurantList };
