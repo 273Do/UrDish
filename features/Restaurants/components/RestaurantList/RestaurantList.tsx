9ff414afe09a68b244f58ab0721bf339dc4935af
273* <114457271+273Do@users.noreply.github.com>
2024-02-13 16:38:08 +0900

    Create RestaurantList.tsx

---
+++ features/Restaurants/components/RestaurantList/RestaurantList.tsx
@@ -0,0 +1,45 @@
+import { restaurantObject } from "@/app/types";
+import Link from "next/link";
+import Image from "next/image";
+
+// レストラン一覧表示のコンポーネント
+const RestaurantList = ({
+  restaurants,
+}: {
+  restaurants: restaurantObject[];
+}) => {
+  return (
+    <div className="m-5 grid grid-cols-1 gap-10 2xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 items-center">
+      {restaurants.map((restaurant: restaurantObject) => (
+        <Link href={`/restaurants/${restaurant.id}`} key={restaurant.id}>
+          <div className="neumorphism flex flex-col p-2 lg:p-4 h-auto w-80 lg:w-full transition-all">
+            <p className="text-base lg:text-xl text-center mb-2">
+              {restaurant.name}
+            </p>
+            <div className="flex mb-2 text-sm lg:text-base">
+              <Image
+                src={restaurant.photo.pc.m}
+                alt={restaurant.name}
+                width={200}
+                height={200}
+                className="w-36 h-36 lg:w-auto lg:h-auto"
+              />
+              <div className="w-72 m-2 mr-0 lg:m-4 opacity-45">
+                <div>
+                  <p>{restaurant.genre.name}</p>
+                  <p>{restaurant.genre.catch}</p>
+                </div>
+                <p className="mt-3 lg:mt-6">{restaurant.catch}</p>
+              </div>
+            </div>
+            <p className="mt-auto text-xs text-center opacity-45">
+              {restaurant.mobile_access}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantList;
