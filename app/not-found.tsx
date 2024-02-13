5e1756779a9d5366dabdfc1911d28cd971e2fbdc
273* <keita.y310@icloud.com>
2024-02-14 00:50:42 +0900

    不必要な要素を削除しました．

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,22 +11,7 @@ const NoData = () => {
         <div className={"card neumorphism mt-40 lg:mt-64 "}>
           <Link href="/">
             <div className="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                height="38px"
-                width="38px"
-                version="1.1"
-                id="heart"
-                viewBox="0 0 471.701 471.701"
-                xmlSpace="preserve"
-              >
-                <linearGradient id="gradientColor">
-                  <stop offset="5%" stop-color="#7eaaff"></stop>
-                  <stop offset="95%" stop-color="#ff48fb"></stop>
-                </linearGradient>
-              </svg>
-              <p className=" -ml-9 text-3xl">:-(</p>
+              <p className="text-3xl">:-(</p>
             </div>
             <p className="title">NotFound</p>
             <p className="text">
--- features/NoData/components/NoData.tsx
+++ features/NoData/components/NoData.tsx
@@ -7,22 +7,7 @@ const NoData = () => {
     <div className={`card neumorphism mt-40 lg:mt-64 ${inter.className}`}>
       <Link href="/">
         <div className="icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            height="38px"
-            width="38px"
-            version="1.1"
-            id="heart"
-            viewBox="0 0 471.701 471.701"
-            xmlSpace="preserve"
-          >
-            <linearGradient id="gradientColor">
-              <stop offset="5%" stop-color="#7eaaff"></stop>
-              <stop offset="95%" stop-color="#ff48fb"></stop>
-            </linearGradient>
-          </svg>
-          <p className=" -ml-9 text-3xl">:-(</p>
+          <p className="text-3xl">:-(</p>
         </div>
         <p className="title">NoData</p>
         <p className="text">
